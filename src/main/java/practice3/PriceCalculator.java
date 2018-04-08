915d63a41b7df7929f00cd965a3fa0dac765d468
llr392425595 <392425595@qq.com>
2018-04-08 09:52:06 +0800

    重构-抽取抽象类

--- src/main/java/practice3/PriceCalculator.java
+++ src/main/java/practice3/PriceCalculator.java
@@ -1,5 +1,7 @@
 package practice3;
 
+import java.math.BigDecimal;
+
 public class PriceCalculator {
 
   private Order order;
@@ -8,5 +10,25 @@ public class PriceCalculator {
     this.order = order;
   }
 
+  public BigDecimal calculate() {
+    BigDecimal subTotal = new BigDecimal(0);
+
+    // Total up line items
+    for (OrderLineItem lineItem : order.orderLineItemList) {
+      subTotal = subTotal.add(lineItem.getPrice());
+    }
+
+    // Subtract discounts
+    for (BigDecimal discount : order.discounts) {
+      subTotal = subTotal.subtract(discount);
+    }
+
+    // calculate tax
+    BigDecimal tax = subTotal.multiply(this.order.tax);
+
+    // calculate GrandTotal
+
+    return subTotal.add(tax);
+  }
 
 }
