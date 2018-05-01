479d0717367520afc89087772da0004b41088870
llr392425595 <392425595@qq.com>
2018-05-01 15:28:45 +0800

    add attribute product for OrderItem

--- src/main/java/practice2/OrderItem.java
+++ src/main/java/practice2/OrderItem.java
@@ -1,19 +1,51 @@
 package practice2;
 
+import java.math.BigDecimal;
+
 public class OrderItem {
-    private Integer count;
-    private long code;
 
-    public OrderItem(long code, int count) {
-        this.code = code;
-        this.count = count;
-    }
+  private Integer count;
+  private long code;
+  private Product product;
 
-    public Integer getCount() {
-        return count;
-    }
+  OrderItem(long code, int count) {
+    this.code = code;
+    this.count = count;
+    this.product = null;
+  }
 
-    public long getCode() {
-        return code;
-    }
+  public OrderItem(Integer count, long code, Product product) {
+    this.count = count;
+    this.code = code;
+    this.product = product;
+  }
+
+  public Integer getCount() {
+    return count;
+  }
+
+  public long getCode() {
+    return code;
+  }
+
+  public Product getProduct() {
+    return product;
+  }
+
+  public void setProduct(Product product) {
+    this.product = product;
+  }
+
+  private BigDecimal getItemTotal() {
+    return this.product == null ? BigDecimal.valueOf(0)
+        : BigDecimal.valueOf(this.count).multiply(this.product.getPrice());
+  }
+
+  private BigDecimal getReducedPrice() {
+    return getItemTotal().multiply(this.product.getDiscountRate());
+  }
+
+  public BigDecimal getDiscountedItemTotal() {
+    return getItemTotal().subtract(getReducedPrice());
+  }
 }
